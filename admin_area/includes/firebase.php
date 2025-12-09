e383a16184946ebfd600e83e89ee291cd6e4c434
madhavanpatel19 <157013470+madhavanpatel19@users.noreply.github.com>
2025-12-09 15:43:47 +0530

    database firebase cannection

--- admin_area/includes/firebase.php
+++ admin_area/includes/firebase.php
@@ -26,7 +26,7 @@ function firebase_db(): Database
 
     // 2) Service account JSON path (override with env if desired)
     $serviceAccount = getenv('FIREBASE_CREDENTIALS')
-        ?: __DIR__ . '/dots-56dac-firebase-adminsdk-fbsvc-5b3075a7b1.json';
+        ?: __DIR__ . '/dots-56dac-firebase-adminsdk-fbsvc-07a558eed5.json';
 
 
     // 3) Realtime Database URL
