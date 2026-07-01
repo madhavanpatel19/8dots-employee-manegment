fc15991e5e39c81632fd58d1d0ebb40f273d6a6b
madhavanpatel19 <157013470+madhavanpatel19@users.noreply.github.com>
2026-07-01 11:25:49 +0530

    set todo path

--- emp_area/ajax_toggle_todo.php
+++ emp_area/ajax_toggle_todo.php
@@ -1,6 +1,8 @@
 <?php
-session_start();
-include(__DIR__ . '/../admin_area/includes/db.php');
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+require_once __DIR__ . '/includes/db.php';
 
 header('Content-Type: application/json');
 
